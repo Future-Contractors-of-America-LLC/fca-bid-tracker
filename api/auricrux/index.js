92a923fad486951d576c73484a826f817b1f4d7c
Auricrux <auricrux@futurecontractorsofamerica.com>
2026-05-23 07:04:52 -0400

    Update index.js

--- api/auricrux/index.js
+++ api/auricrux/index.js
@@ -1,9 +1,16 @@
-export default async function (context, req) {
-  context.res = {
-    status: 200,
-    body: {
-      ok: true,
-      message: "Auricrux API ONLINE"
-    }
-  };
-}
+import { app } from "@azure/functions";
+
+app.http("auricrux", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  route: "auricrux",
+  handler: async (request, context) => {
+    return {
+      status: 200,
+      jsonBody: {
+        ok: true,
+        message: "Auricrux API ONLINE"
+      }
+    };
+  }
+});
