b78cb69f397dedecaa66387a93408ed4af5a0ed7
Auricrux <auricrux@futurecontractorsofamerica.com>
2026-05-28 23:13:46 -0400

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div>
+      <h2>Login to FCA</h2>
+
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+
+      <button>Login</button>
+    </div>
+  );
+}
