e53139ae9c048c3a42e504494a9e417057108b91
Auricrux <auricrux@futurecontractorsofamerica.com>
2026-05-28 23:23:40 -0400

    Create page.tsx

---
+++ app/demo/page.tsx
@@ -0,0 +1,15 @@
+export default function Demo() {
+  return (
+    <div>
+      <h1>FCA Demo Mode</h1>
+
+      <p>This is a live preview of the FCA system.</p>
+
+      <ul>
+        <li><a href="/">Landing</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/dashboard">Dashboard</a></li>
+      </ul>
+    </div>
+  );
+}
