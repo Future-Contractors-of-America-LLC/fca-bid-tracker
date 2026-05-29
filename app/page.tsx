6de6093907cfb2953bef59108bfcfe641f6dbca7
Auricrux <auricrux@futurecontractorsofamerica.com>
2026-05-28 23:13:13 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Future Contractors of America</h1>
+      <p>The operating system for construction.</p>
+
+      <a href="/login">
+        <button>Enter FCA</button>
+      </a>
+    </div>
+  );
+}
