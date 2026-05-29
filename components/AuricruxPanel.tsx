3d254c9ee95176a3817ecb8e7e6b8d5d68440a49
Auricrux <auricrux@futurecontractorsofamerica.com>
2026-05-28 23:15:50 -0400

    Create AuricruxPanel.tsx

---
+++ components/AuricruxPanel.tsx
@@ -0,0 +1,9 @@
+export default function AuricruxPanel() {
+  return (
+    <div style={{border: '1px solid #ccc', padding: '10px'}}>
+      <h3>Auricrux</h3>
+      <input placeholder="Ask Auricrux..." />
+      <button>Send</button>
+    </div>
+  );
+}
