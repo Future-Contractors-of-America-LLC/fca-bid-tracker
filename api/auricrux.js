ebef58ec9719abc0d8493899753a8fadf936203d
michael <live.com#baguba80@gmail.com>
2026-06-11 13:59:03 +0000

    stabilize: generate required SWA smoke files and API stubs

--- api/auricrux.js
+++ api/auricrux.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
+    headers: { "Content-Type": "text/plain" },
     body: "auricrux endpoint OK"
   };
 };
-// CI FORCE Thu Jun 11 01:29:18 PM UTC 2026
