1f934a933905cc9f777444cbab4e08e45d1e164c
MICHAEL J BARTHOLOMEW <auricrux@futurecontractorsofamerica.com>
2026-06-17 16:17:50 -0400

    frontend cleanup: harden app bootstrap and load global stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "./index.css";
 import Router from "./router";
 import AuricruxDock from "./components/AuricruxDock.jsx";
 import RootErrorBoundary from "./components/RootErrorBoundary.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error('Application root element with id "root" was not found.');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <RootErrorBoundary>
       <Router />
       <AuricruxDock />
     </RootErrorBoundary>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
